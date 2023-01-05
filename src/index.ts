4a885263e6dac75731780bae150ec8bf4a4f5269
Miguel Penha <miguel.penha1@gmail.com>
2023-01-05 08:45:33 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -12,15 +12,7 @@ app.get('/:number/messages', async (req, res) => {
     res.json({ send: true })
 })
 
-app.get('/webhooks', (req: Request<{}, {}, {}, { 'hub.verify_token': string, 'hub.challenge': number }>, res) => {
-    /*
-    const { 'hub.verify_token': verifyToken, 'hub.challenge': challenge } = req.query
-
-    if (verifyToken === process.env.VERIFY_TOKEN) {
-        res.send(challenge)
-    }
-    */
-
+app.post('/webhooks', (req: Request<{}, {}, {}, { 'hub.verify_token': string, 'hub.challenge': number }>, res) => {
     console.log(req.body)
 })
 
