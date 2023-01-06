dfdf886150240527050ffb75e585c0a5cc466a7b
Miguel Penha <miguel.penha1@gmail.com>
2023-01-05 22:47:54 -0300

    Update index.ts

--- src/messages/index.ts
+++ src/messages/index.ts
@@ -6,12 +6,12 @@ import messageInteractive from './messageInteractive'
 import messageReply from './messageReply'
 
 async function messages(phoneToReceive: number) {
-    await messageBoleto(phoneToReceive, new Date().toLocaleDateString('pt-br'), 'boletos/boleto.pdf')
-    await messageReaction(phoneToReceive, '\uD83D\uDE00', 'wamid.HBgMNTU4MTgzNzA1Nzc1FQIAERgSNjcyNDAxMTE0RTg2RkNCMzBBAA==')
-    await messageMedia(phoneToReceive, 'https://images.unsplash.com/photo-1672855570131-fa10d437bc86?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8')
-    await messageLocation(phoneToReceive, -7.9600057, -34.8925448, 'Instituto Educacional Josival Penha', 'Av. João Paulo II, 894 - Mirueira, Paulista - PE, 53405-190')
-    await messageInteractive(phoneToReceive)
-    await messageReply(phoneToReceive, 'wamid.HBgMNTU4MTgzNzA1Nzc1FQIAERgSNjcyNDAxMTE0RTg2RkNCMzBBAA==')
+    console.log(await messageBoleto(phoneToReceive, new Date().toLocaleDateString('pt-br'), 'boletos/boleto.pdf'))
+    console.log(await messageReaction(phoneToReceive, '\uD83D\uDE00', 'wamid.HBgMNTU4MTgzNzA1Nzc1FQIAERgSNjcyNDAxMTE0RTg2RkNCMzBBAA=='))
+    console.log(await messageMedia(phoneToReceive, 'https://images.unsplash.com/photo-1672855570131-fa10d437bc86?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8'))
+    console.log(await messageLocation(phoneToReceive, -7.9600057, -34.8925448, 'Instituto Educacional Josival Penha', 'Av. João Paulo II, 894 - Mirueira, Paulista - PE, 53405-190'))
+    console.log(await messageInteractive(phoneToReceive))
+    console.log(await messageReply(phoneToReceive, 'wamid.HBgMNTU4MTgzNzA1Nzc1FQIAERgSNjcyNDAxMTE0RTg2RkNCMzBBAA=='))
 }
 
 export default messages
